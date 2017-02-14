864e279c2b627e3b693c8d1efc5ae0f987efbd2b
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2017-02-14 20:45:05 +0900

    change xml configuration to javaconfig

---
+++ src/main/java/x3/benjamin/playground/apiserver/config/RootApplicationContextConfig.java
@@ -0,0 +1,10 @@
+package x3.benjamin.playground.apiserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by benjamin on 2017. 2. 14..
+ */
+@Configuration
+public class RootApplicationContextConfig {
+}
---
+++ src/main/java/x3/benjamin/playground/apiserver/config/ServletContextConfig.java
@@ -0,0 +1,12 @@
+package x3.benjamin.playground.apiserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by benjamin on 2017. 2. 14..
+ */
+@Configuration
+@EnableWebMvc
+public class ServletContextConfig {
+}
