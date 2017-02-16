da3fa0cc4094f2e497ff4efd9df5441ece37149e
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2017-02-17 00:29:28 +0900

    add customized JacksonHttpMessageConverter

--- src/main/java/x3/benjamin/playground/apiserver/config/ServletContextConfig.java
+++ src/main/java/x3/benjamin/playground/apiserver/config/ServletContextConfig.java
@@ -1,9 +1,17 @@
 package x3.benjamin.playground.apiserver.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * Created by benjamin on 2017. 2. 14..
@@ -12,4 +20,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan(basePackages = {"x3.benjamin.playground.apiserver.controller"})
 public class ServletContextConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jackson2HttpMessageConverter());
+        super.configureMessageConverters(converters);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new Jdk8Module());
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
 }
