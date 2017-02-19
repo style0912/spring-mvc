13474619dacd971508d65367864a92ea5e68be05
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2017-02-19 21:25:50 +0900

    properties

--- src/main/java/x3/benjamin/playground/apiserver/config/RootApplicationContextConfig.java
+++ src/main/java/x3/benjamin/playground/apiserver/config/RootApplicationContextConfig.java
@@ -1,7 +1,14 @@
 package x3.benjamin.playground.apiserver.config;
 
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
 
 /**
  * Created by benjamin on 2017. 2. 14..
@@ -12,4 +19,12 @@ import org.springframework.context.annotation.Configuration;
         "x3.benjamin.playground.apiserver.service"
 })
 public class RootApplicationContextConfig {
+
+    @Bean(name = "system")
+    public PropertiesFactoryBean propertiesFactoryBean() throws IOException {
+        PropertiesFactoryBean bean = new PropertiesFactoryBean();
+        bean.setLocation(new ClassPathResource("/properties/system.properties"));
+        bean.afterPropertiesSet();
+        return bean;
+    }
 }
--- src/main/java/x3/benjamin/playground/apiserver/service/impl/UserServiceImpl.java
+++ src/main/java/x3/benjamin/playground/apiserver/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package x3.benjamin.playground.apiserver.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import x3.benjamin.playground.apiserver.model.User;
 import x3.benjamin.playground.apiserver.repository.UserRepository;
@@ -14,12 +15,15 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Value("#{system['api.log.host']}")
+    private String host;
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public List<User> getUsers() {
-        System.out.println("Service Layer - users method is called");
+        System.out.println("Service Layer - users method is called. api.log.host=" + host);
         return userRepository.findAll();
     }
 }
